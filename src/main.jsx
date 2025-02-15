2ce59c9c5d9ec8cae0f3f0ef086a601a9900dd16
smb-lemon <smblemon9@gmail.com>
2025-02-16 02:03:33 +0600

    style: update h4 and button style in Newsletter fix: resolve main by removing extra import

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 import './index.css';
-import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
