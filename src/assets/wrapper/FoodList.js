5075510b7bc52e789e0271ea37352d475b0e2896
smb-lemon <smblemon9@gmail.com>
2025-02-24 11:08:17 +0600

    style: updated FoodList with content alignment and better transitions - .card div updated with     - adding flex-direction,display,align-items,transitions     - removing justify-content, flex-wrap, padding, width - .card div hover updated with new values - h3 updated with     - removing font-size     - adding color, text-align - .img updated with object-fit - a:hover updated with color

--- src/assets/wrapper/FoodList.js
+++ src/assets/wrapper/FoodList.js
@@ -9,53 +9,67 @@ const Wrapper = styled.div`
         margin-bottom: 20px;
         letter-spacing: 2px;
     }
-   .container-div {
+
+    .container-div {
         max-width: var(--max-width);
         margin: auto;
         padding: 20px;
         text-align: center;
         display: flex;
         flex-wrap: wrap;
+        justify-content: center;
         gap: 20px;
-   }
+    }
+
     .card-div {
         border: 1px solid #ddd;
-        padding: 10px; 
-        width: 250px;
-        border-radius: var(--borderRadius);
-        justify-content: center;
-        flex-wrap: wrap;
         padding: 15px;
-        transition: transform 0.2s;
+        width: 280px;
+        border-radius: var(--borderRadius);
         background: var(--background);
-        //box-shadow: 0 4px 6px rgba(0,0,0,0,1);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
     }
+
     .card-div:hover {
-        transform: scale(1.03);
-        box-shadow: 0 4px 6px rgba(0,0,0,0,1);
+        transform: scale(1.05);
+        box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
     }
+
     h3 {
-        font-size: 25px;
+        font-size: 1.6rem;
         margin: 10px 0;
+        color: var(--primary-600);
+        text-align: center;
     }
+
     .img {
         width: 100%;
         border-radius: 10px;
+        object-fit: cover;
     }
+
+
     p {
-        font-size: 15px;
+        font-size: 14px;
         margin-top: 0.4rem;
         color: var(--grey-500);
     }
+
     a {
         display: block;
         margin-top: 10px;
         text-decoration: none;
         color: var(--primary-500);
         font-weight: bold;
+        transition: color 0.2s ease-in-out;
     }
+
     a:hover {
         text-decoration: underline;
+        color: var(--primary-700);
     }
 `;
 
