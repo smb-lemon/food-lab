79d5dd8d9243978db68f88d0d413dcbcf9fcd07e
smb-lemon <smblemon9@gmail.com>
2025-02-24 14:23:44 +0600

    feat: added alternative text for searching food - readyInMinutes ?? N/A : N/A will show if the time data not found - servings ?? N/A : N/A will print if the serving time data not found - if the sourceUrl unavailable, it will show the p tag

--- src/components/Foodlist.jsx
+++ src/components/Foodlist.jsx
@@ -11,9 +11,13 @@ const Foodlist = ({recipes}) => {
           <div key={recipe.id} className='card-div'>
             <h3>{recipe.title}</h3>
             <img src={recipe.image} alt={recipe.title} className='img' />
-            <p><strong>Time:</strong> {recipe.readyInMinutes} mins</p>
-            <p><strong>Servings:</strong> {recipe.servings}</p>
-            <a href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer">View Recipe</a>
+            <p><strong>Time:</strong> {recipe.readyInMinutes ?? "N/A"} mins</p>
+            <p><strong>Servings:</strong> {recipe.servings ?? "N/A"}</p>
+            {recipe.sourceUrl ? ( 
+              <a href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer">View Recipe</a>
+                ):(
+                    <p>Full Details Unavailable</p>
+                  )}
           </div>
         ))}
       </div>
